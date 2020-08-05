a1c630d027c6fbebdf6333d726241f932ffecc26
Carolina Guzman <carolinaguzman76@gmail.com>
2020-08-05 19:51:43 +0200

    primera subida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BusquedaComponent
+    
   ],
   imports: [
-    BrowserModule
+  BrowserModule,
+  NgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/busqueda/busqueda.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'busqueda',
+    templateUrl: './busqueda.component.html'
+
+})
+
+export class BusquedaComponent {
+
+}
+
+
+
