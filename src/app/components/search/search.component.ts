6b09ac3d48bc1649d65787d76ebdd05610c610e2
Carolina Guzman <carolinaguzman76@gmail.com>
2020-08-11 19:30:26 +0200

    acatacion de fechas validas

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgbDate } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-date'
 import { SearchService } from '../../services/search.service';
 import { AsteroidsNeowsModel } from '../../model/asteroidsNeowsModel';
+import { NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-search',
@@ -10,12 +11,17 @@ import { AsteroidsNeowsModel } from '../../model/asteroidsNeowsModel';
 })
 export class SearchComponent implements OnInit {
 
+  initialDate: NgbDate;
+  finalDate: NgbDate;
+
   newInitialDate: string;
   newFinalDate: string;
 
+
+
   asteroids: AsteroidsNeowsModel;
 
-  constructor(private searchService: SearchService) {
+  constructor(private searchService: SearchService, private calendar: NgbCalendar) {
   }
 
   ngOnInit(): void {
@@ -36,12 +42,34 @@ export class SearchComponent implements OnInit {
 
   dateFrom(fecha: NgbDate): void {
     this.newInitialDate = fecha.year + '-' + fecha.month + '-' + fecha.day;
+    this.initialDate = fecha;
 
   }
 
   dateTo(fecha: NgbDate): void {
     this.newFinalDate = fecha.year + '-' + fecha.month + '-' + fecha.day;
 
+  }
+
+  getMaxDate(): NgbDateStruct {
+
+    if (this.initialDate) {
+
+      const finalMaxDate: NgbDate = this.calendar.getNext(this.initialDate, 'd', 7);
+      const newMaxDate: NgbDateStruct = { day: finalMaxDate.day, month: finalMaxDate.month, year: finalMaxDate.year };
+      return newMaxDate;
+
+    }
+    return undefined;
+  }
+
+  getMinDate(): NgbDateStruct {
+
+    if (this.initialDate) {
+
+      const finalMinDate: NgbDateStruct = {day: this.initialDate.day, month: this.initialDate.month, year: this.initialDate.year};
+      return finalMinDate;
+    }
   }
 
 }
